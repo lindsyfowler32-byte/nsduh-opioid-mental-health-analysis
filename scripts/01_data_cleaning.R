5ef2493df170a03cdbfd32caab884c7a2bc798c2
Lindsy Fowler <lindsyfowler32@gmail.com>
2026-01-08 19:21:09 -0500

    Refactor data cleaning with tidyverse functions
    Refactor data cleaning process to use tidyverse functions for selection, mutation, and filtering.

--- scripts/01_data_cleaning.R
+++ scripts/01_data_cleaning.R
@@ -1,21 +1,6 @@
-library(...)
+library(tidyverse)
 
-select()
-
-mutate()
-
-filter()
-
-drop_na()
-
-creation of:
-
-opioid_binary
-
-mental_fog
-
-opioid_user
-
-mh_distress
-
-health_score
+analysis_df <- ds %>%
+  select(...) %>%
+  mutate(...) %>%
+  filter(...)
